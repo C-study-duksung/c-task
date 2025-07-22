504ed5586b7e83eb53ee7797bd1ac0c3474782f0
hyojung <jhjungj2005@gmail.com>
2025-07-23 00:21:17 +0900

    test

---
+++ week 2/week2task1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// 점수 입력 받으면 학점 출력
+// wntjr
+
+int main(void){
+
+    // 점수 변수 선언
+    int num;
+    
+    // 점수 입력 받기
+    scanf("%d, &num");
+
+    // 입력 받은 점수에 따른 학점 판별 조건문
+    if(90 <= num <= 100)
+        printf("A")
+    else if(80 <= num <= 89)
+        printf("B")
+    else if(70 <= num < 79)
+        printf("C")
+    else if(60 <= num <= 69)
+        printf("D")
+    else
+        printf("F")
+
+
+}
+
+/* 
+    1. 점수 조건에 따라 학점을 받는 변수 grade는 따로 설정할 필요 없는건지?
+       점수 조건에 따라 grade를 다르게 저장되게(?)해서 그 grade를 출력하는 방법도 생각해봤음
+
+*/
