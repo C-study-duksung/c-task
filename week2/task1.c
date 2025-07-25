47b8f23451b4b73ba21ee80da1aad59f99cee6dc
unknown <hyunyiroh@gmail.com>
2025-07-25 23:43:29 +0900

    task1

--- week2/task1.c
+++ week2/task1.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
-int main(void){
+int main() {
+	int score;
 
-  
+        for(;;) {
+            printf("점수 기입하시오 : ");
+            scanf("%d", &score);
+
+            if (score <0 || score >100) {
+                printf("다시 기입하시오\n");
+            }
+            else {
+            if ( score >= 90) {
+                printf("A\n");
+            } else if (score >= 80) {
+                printf("B\n");
+            } else if (score >= 70) {
+                printf("C\n"); 
+            } else if (score >= 60) {
+                printf("D\n");
+            } else {
+                printf("F\n");
+            }
+        
+            break;
+        }
+    }
+    return 0;
 }
