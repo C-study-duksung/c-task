adeba883372ca829f33f586ef3fe4af346a6846e
hyojung <jhjungj2005@gmail.com>
2025-07-28 16:34:05 +0900

    2주차 과제 중

--- week 2/task7.c
+++ week 2/task7.c
@@ -13,16 +13,29 @@ int main(void)
     // 1 <= a, b, c <= 6 아닐 경우, 다시 입력 받기
     a = 0, b = 0, c = 0;
     while (a < 1 || a > 6 || b < 1 || b > 6 || c < 1 || c > 6)
-        scanf("%d, %d, %d", &a, &b, &c);
+        scanf("%d %d %d", &a, &b, &c);
 
     // if 문 이용
     if (a == b && b == c) // 눈 3개 동일
-        money = 10000 + a*1000
+        money = 10000 + a*1000;
     else if (a == b || b == c || a == c) // 눈 2개 동일
-        // 같은 눈 판별
-        money = 1000 + (같은 눈)*100
+    {
+        if (a == b || b == c)
+            money = 1000 + b*100;
+        else
+            money = 1000 + a*100;
+    }
+    else // 눈 모두 다름
+        if (a > b && a > c)
+            money = a * 100;
+        else if (b > a && b > c)
+            money = b * 100;
+        else  
+            money = c*100;
 
-    // 눈 1개 동일
+    // 상금 출력
+    printf("%d", money);
 
+    return 0;
 
 }
