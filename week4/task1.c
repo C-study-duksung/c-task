8729b32977c44ed4f1289db560967961829b4c26
hyojung <jhjungj2005@gmail.com>
2025-08-07 01:24:41 +0900

    4주차 과제 중

---
+++ week4/task1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// 2배 증가 함수 선언
+void ChangeDouble(*pnum);
+
+// main 함수
+int main(void){
+
+    // 변수 선언
+    int num;
+    int DoubleNum;
+    int *pnum;
+    pnum = &num;
+    
+    // 변수 입력받기
+    printf("숫자 입력 : ");
+    scanf("%d", &num);
+
+    // 2배 증가 함수 호출
+    DoubleNum = ChangeDouble(num);
+    printf("%d", DoubleNum);
+
+    return 0;
+
+}
+
+// 2배 증가 함수 정의
+int ChangeDouble(*pnum){
+
+    (*pnum) *= 2;
+
+    return *pnum;
+
+}
